5398258638c3c8c7fc4bcf34c5591e6c81d4335f
oliverlangmo <oliver.langmo@gmail.com>
2015-08-03 18:53:42 -0500

    with commented code

---
+++ script.js
@@ -0,0 +1,43 @@
+// Code goes here
+
+angular.module('CORSDemo', [])
+.config(function($httpProvider) {
+	$httpProvider.defaults.useXDomain = true;
+})
+.controller('MyController', function($scope, $http, $sce) {
+	
+  $scope.embedUrl = "http://www.youtube.com/embed/";
+		
+	$scope.trustSrc = function(src) {
+	  return $sce.trustAsResourceUrl(src);
+	};//strict contextual escaping
+
+	$scope.searchYouTube = function(keyword) {
+			
+  	$scope.keyword = keyword;
+
+	  var url = "https://www.googleapis.com/youtube/v3/search";
+	  var request = {
+		  key: "AIzaSyBIT2homLIII4fhIFO1ePwQDKA_rm2Ym30",
+		  part: "snippet",
+		  maxResults: 10,
+		  order: "viewCount",
+		  q: keyword,
+		  type: "video",
+		  videoEmbeddable: true,
+	  };//search parameters
+
+	  $http({
+		  method: 'GET',
+		  url: url,
+		  params: request
+	  })
+	  .success(function(data) {
+		  $scope.results = data.items;
+	  })
+	  .error(function() {
+		  alert('error');
+	  });
+  };
+
+});
